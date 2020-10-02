1ba85e576305093c4f7198045828ba50d4c70558
tykita <4rchive7@gmail.com>
2020-10-02 09:17:52 +0900

    Initial commit

---
+++ src/main/java/com/tykita/toy_01_onlineboard/Toy01OnlineboardApplication.java
@@ -0,0 +1,14 @@
+package com.tykita.toy_01_onlineboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Toy01OnlineboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Toy01OnlineboardApplication.class, args);
+
+	}
+
+}
